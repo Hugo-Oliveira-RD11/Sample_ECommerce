99f4622d76b7a61d2f410c464e0486251b0819ae
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2021-08-21 23:05:03 -0300

    mandando o EFCore criar novos bancos de dados

--- Sample_ECommerce.Api/Models/Data/AppDataContext.cs
+++ Sample_ECommerce.Api/Models/Data/AppDataContext.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using Sample_ECommerce.Api.Models.Produto;
+using Sample_ECommerce.Api.Models.Fornecedor;
 
 namespace Sample_ECommerce.Api.Models
 {
@@ -8,6 +10,13 @@ namespace Sample_ECommerce.Api.Models
       public AppDataContext (DbContextOptions<AppDataContext> op) : base(op){}
 
       public DbSet<Cliente_Cliente> Clientes { get; set; }
+      public DbSet<Fornecedor_Fornecedor> Fornecedores { get; set; }
+      public DbSet<Produto_Produto> Produtos { get; set; }
+      public DbSet<Produto_Console> Consoles { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+// definir a precição do preco!
+    }
   }
 }
