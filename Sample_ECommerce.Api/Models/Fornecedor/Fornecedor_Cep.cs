e44c175aba534120e98eb7aedac3b7607d4d084c
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2021-08-21 23:03:40 -0300

    Adicionando um novo conjunto de tabelas para o fornecedor

---
+++ Sample_ECommerce.Api/Models/Fornecedor/Fornecedor_Cep.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sample_ECommerce.Api.Models.Fornecedor
+{
+    public  class Fornecedor_Cep
+    {
+        public int Id { get; set; }
+        public string CEP { get; set; }
+        public string Endereco { get; set; }
+        public string NumeroResidencia { get; set; }
+        public string Complemento { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.DateTime)]
+        public DateTime ModifiedDate { get; set; }
+        public int FornecedorId { get; set; }
+        public Fornecedor_Fornecedor Fornecedor { get; set; }
+    }
+}
