3ae1a3588787c863483575a31cd1038c4130a251
Hugo de Oliveira Resende <hugo.resennde1123@gmail.com>
2021-08-21 22:58:43 -0300

    modificando o nome da conexao

--- Sample_ECommerce.Api/Startup.cs
+++ Sample_ECommerce.Api/Startup.cs
@@ -27,7 +27,7 @@ namespace Sample_ECommerce.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDataContext>(op => op.UseSqlServer(Configuration.GetConnectionString("Bank")));
+            services.AddDbContext<AppDataContext>(op => op.UseSqlServer(Configuration.GetConnectionString("SqlServer")));
             services.AddControllers();
         }
 
